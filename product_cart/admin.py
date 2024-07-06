5f51548a6616ab383673d7cd99b4eba5fe875c33
justin <justinemsengi@gmail.com>
2024-07-06 21:50:20 +0300

    Updated into the current django version

---
+++ product_cart/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Cart, CartProduct
+
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('cart_id', 'created_at')
+    readonly_fields = ('cart_id', 'created_at')
+    list_per_page = 5
+
+
+class CartProductAdmin(admin.ModelAdmin):
+    list_display = ('product', 'quantity')
+    readonly_fields = ('product', 'quantity', 'cart', 'status', 'user', 'variation')
+    list_per_page = 5
